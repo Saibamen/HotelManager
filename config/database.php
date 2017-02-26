e03c2537ca3460851f2f81d4ac4bba0314e8a410
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2017-02-26 18:32:03 +0100

    **Ostatnie** StyleCI

--- config/database.php
+++ config/database.php
@@ -122,9 +122,9 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
 
