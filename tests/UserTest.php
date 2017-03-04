b2c6caf54f146887f419020c6936a4523a282c8a
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2017-03-04 18:27:25 +0100

    Pierwsze testy dla logowania ^^

---
+++ tests/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class UserTest extends TestCase
+{
+
+    public function testEmptyLogin() {
+        $this->visit('/login')
+            ->see('Zaloguj')
+            ->type('', 'email')
+            ->type('', 'password')
+            ->press('Zaloguj')
+            ->seePageIs('/login')
+            ->dontSee('Cześć')
+            ->see('Pole adres e-mail jest wymagane.')
+            ->see('Pole hasło jest wymagane.');
+    }
+
+    public function testSimpleFailLogin() {
+        $this->visit('/login')
+            ->see('Zaloguj')
+            ->type('badEmail', 'email')
+            ->type('badPass', 'password')
+            ->press('Zaloguj')
+            ->seePageIs('/login')
+            ->dontSee('Cześć')
+            ->see('Błędny login lub hasło.');
+    }
+}
