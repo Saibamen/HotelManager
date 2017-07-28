918aa80bea95c22dad40830decbf0359f68e7b90
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2017-07-28 16:20:15 +0200

    Composer update, pliki tekstowe z imionami, miastami i nazwiskami, uwagami i ulicami dodanie pre tytułu do title taga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $title }} - {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css') !!}
