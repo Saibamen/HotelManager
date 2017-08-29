6cf4e39f182edf5815bfc82956588c62acea9714
Saibamen <saibamenppl@gmail.com>
2017-08-29 17:04:35 +0200

    Apply fixes from StyleCI (#13)

--- resources/lang/en/navigation.php
+++ resources/lang/en/navigation.php
@@ -5,10 +5,10 @@ return [
     'add_room'                 => 'Add room',
     'currently_free_rooms'     => 'Currently free rooms',
     'currently_occupied_rooms' => 'Currently occupied rooms',
-    
+
     'show_guests' => 'Show guests',
     'add_guest'   => 'Add guest',
-    
+
     'all_reservations'     => 'All reservations',
     'current_reservations' => 'Current reservations',
     'future_reservations'  => 'Future reservations',
