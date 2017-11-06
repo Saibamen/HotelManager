e4785b7cbaec87430b8a5e406b7e48ae1c9253bd
Adam Stachowicz <saibamenppl@gmail.com>
2017-11-06 23:35:54 +0100

    WIP simple GuestTest

---
+++ tests/GuestTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use App\Models\Guest;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class GuestTest extends BrowserKitTestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Session::start();
+
+        $user = factory(App\Models\User::class)->create();
+        $this->actingAs($user);
+    }
+
+    public function testEmptyIndex()
+    {
+        $this->visit('guest')
+            ->dontSee('Zaloguj')
+            ->see('Goście')
+            ->dontSee('Imię')
+            ->dontSee('Nazwisko')
+            ->dontSee('Adres')
+            ->dontSee('Kod pocztowy')
+            ->dontSee('Miejscowość')
+            ->dontSee('PESEL')
+            ->dontSee('Kontakt')
+            ->dontSee('Akcje')
+            ->dontSee('Edytuj')
+            ->dontSee('Usuń')
+            ->see('Dodaj');
+    }
+
+    public function testAddEmptyForm()
+    {
+        $this->visit('guest/add')
+            ->dontSee('Zaloguj')
+            //->see('Dodaj pokój')
+            ->see('Imię')
+            ->see('Nazwisko')
+            ->see('Pojemność')
+            ->see('Adres')
+            ->see('Kod pocztowy')
+            ->see('Miejscowość')
+            ->see('PESEL')
+            ->see('Kontakt')
+            ->see('Wyślij')
+            ->press('Wyślij');
+
+        //$this->seePageIs('guest/add')
+        //->see('jest wymagane');
+    }
+
+    public function testTryEditInvalidId()
+    {
+        $this->visit('guest')
+            ->see('Goście')
+            ->visit('guest/edit/10000');
+
+        $this->followRedirects();
+
+        $this->see('Nie znaleziono obiektu');
+    }
+
+    public function testTryStoreInvalidId()
+    {
+        $this->makeRequest('POST', 'guest/edit/1000', [
+            '_token' => csrf_token(),
+        ]);
+
+        $this->notSeeInDatabase('guest', []);
+    }
+}
