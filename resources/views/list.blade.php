694e0ca88e10b2ace1a3033e9b766ce4e5813bb2
Adam Stachowicz <saibamenppl@gmail.com>
2017-11-27 23:28:00 +0100

    Change container column size to md-12

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{ $title or null }}</div>
 
