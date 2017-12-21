647512bd93d4c6b8223576e4dcae22ee0428de5b
Adam Stachowicz <saibamenppl@gmail.com>
2017-12-21 19:47:38 +0100

    Just to test new validation in CodeClimate part 2/x

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ let mix = require("laravel-mix");
 mix.js("resources/assets/js/app.js", "public/js/app.min.js")
    .sass("resources/assets/sass/app.scss", "public/css/app.min.css");
 
+// My mixes
 mix.js("resources/assets/js/deletemodal.js", "public/js/deletemodal.min.js");
 
 // Cache busting
