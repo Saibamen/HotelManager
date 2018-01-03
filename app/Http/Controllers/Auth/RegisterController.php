e024e12ed05fd21d4b30d19f36eefed020bb64cf
Adam Stachowicz <saibamenppl@gmail.com>
2018-01-03 22:26:40 +0100

    Fix return type adnotation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
      *
      * @param array $data
      *
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
