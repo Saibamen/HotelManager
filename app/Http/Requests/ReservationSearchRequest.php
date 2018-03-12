af0440d04332735078cfed89947a083db50bde4e
Adam Stachowicz <saibamenppl@gmail.com>
2018-03-12 02:13:59 +0100

    Better messages about dates validation

--- app/Http/Requests/ReservationSearchRequest.php
+++ app/Http/Requests/ReservationSearchRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReservationSearchRequest extends FormRequest
@@ -13,10 +14,14 @@ class ReservationSearchRequest extends FormRequest
 
     public function rules()
     {
+        $carbon = new Carbon();
+        $yesterday = $carbon->yesterday()->toDateString();
+        $today = $carbon->today()->toDateString();
+
         return [
             'guest'        => 'required',
-            'date_start'   => 'required|date|after:yesterday|before:date_end',
-            'date_end'     => 'required|date|after:today|after:date_start',
+            'date_start'   => 'required|date|after:'.$yesterday.'|before:date_end',
+            'date_end'     => 'required|date|after:'.$today.'|after:date_start',
             'people'       => 'required|numeric|min:1|digits_between:1,2',
         ];
     }
