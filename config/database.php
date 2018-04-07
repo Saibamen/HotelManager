0e9d89ea7d5f0bbd252bc16c42c10ac960e603b3
Adam Stachowicz <adam.stachowicz@fingo.pl>
2018-04-07 21:51:33 +0200

    Fixes

--- config/database.php
+++ config/database.php
@@ -56,10 +56,10 @@ return [
 
         'testing' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'host'      => env('DB_TEST_HOST', '127.0.0.1'),
             'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
             'username'  => env('DB_TEST_USERNAME', 'homestead'),
-            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
