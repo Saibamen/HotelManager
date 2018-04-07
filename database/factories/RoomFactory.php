4085983eab541d2173a4e7379b5a5c24b56d4d2d
Saibamen <saibamenppl@gmail.com>
2018-04-07 23:17:06 +0200

    Fix factory for Room

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Models\Room::class, function (Faker $faker) {
     return [
         'number'   => $faker->numerify(),
         'floor'    => $faker->numerify(),
-        'capacity' => $faker->numerify('##'),
+        'capacity' => $faker->numberBetween(1, 99),
         'price'    => $faker->numerify(),
         'comment'  => 'test comment',
     ];
