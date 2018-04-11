11884823cc2d4a16486bfa9ccf5da3d1cf10fee8
Saibamen <saibamenppl@gmail.com>
2018-04-11 23:33:54 +0200

    Extend some columns in guests table

--- database/migrations/2017_01_21_143900_create_guests_table.php
+++ database/migrations/2017_01_21_143900_create_guests_table.php
@@ -10,11 +10,11 @@ class CreateGuestsTable extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name', 20);
-            $table->string('last_name', 40);
+            $table->string('first_name', 35);
+            $table->string('last_name', 45);
             $table->string('address', 50);
-            $table->string('zip_code', 6);
-            $table->string('place', 30);
+            $table->string('zip_code', 12);
+            $table->string('place', 35);
             $table->string('PESEL', 11);
             $table->string('contact')->nullable();
             $table->timestamp('created_at')->useCurrent();
