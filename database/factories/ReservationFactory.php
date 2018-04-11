bd43dfc5b48d20d49e094beaa806f3612aec2c0b
Adam Stachowicz <saibamenppl@gmail.com>
2018-04-11 22:15:38 +0000

    Apply fixes from StyleCI

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Models\Reservation::class, function (Faker $faker) use ($factory)  {
+$factory->define(App\Models\Reservation::class, function (Faker $faker) use ($factory) {
     return [
         'room_id'     => $factory->create(App\Models\Room::class)->id,
         'guest_id'    => $factory->create(App\Models\Guest::class)->id,
