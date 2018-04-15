9a12b4ced1a39bde62a56ea799561ad40cfa0cae
Adam Stachowicz <adam.stachowicz@fingo.pl>
2018-04-16 00:19:48 +0200

    Doublequotes in JS

--- resources/assets/js/datepickersettings.js
+++ resources/assets/js/datepickersettings.js
@@ -1,11 +1,11 @@
 var generalSettings = {
     todayHighlight: true,
     autoclose: true,
-    format: 'dd.mm.yyyy'
+    format: "dd.mm.yyyy"
 };
 
-var startDateSettings = $.extend({}, generalSettings, { startDate: '0d', todayBtn: 'linked' });
-$('.datepicker.start-date').datepicker(startDateSettings);
+var startDateSettings = $.extend({}, generalSettings, { startDate: "0d", todayBtn: "linked" });
+$(".datepicker.start-date").datepicker(startDateSettings);
 
-var endDateSettings = $.extend({}, generalSettings, { startDate: '+1d' });
-$('.datepicker.end-date').datepicker(endDateSettings);
+var endDateSettings = $.extend({}, generalSettings, { startDate: "+1d" });
+$(".datepicker.end-date").datepicker(endDateSettings);
