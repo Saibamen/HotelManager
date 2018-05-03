28c9149e012d3a48f431289bcffc81c78b8ca703
Saibamen <saibamenppl@gmail.com>
2018-05-03 14:34:15 +0200

    Translated e-mail template

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,57 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level == 'error')
+# Whoops!
+@else
+# @lang('general.mail_greeting')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+            $color = 'green';
+            break;
+        case 'error':
+            $color = 'red';
+            break;
+        default:
+            $color = 'blue';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('general.mail_regards'),<br>{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@component('mail::subcopy')
+@lang('general.mail_action_trouble_1') "{{ $actionText }}"@lang('general.mail_action_trouble_2') [{{ $actionUrl }}]({{ $actionUrl }})
+@endcomponent
+@endisset
+@endcomponent
