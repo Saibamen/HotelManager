400d0b50dfab17c3b322e1910eed86b56d37edeb
Saibamen <saibamenppl@gmail.com>
2018-05-03 15:17:32 +0200

    Translated reset password e-mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -47,4 +48,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends \Illuminate\Auth\Notifications\ResetPassword
+{
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(trans('passwords.mail_subject'))
+            ->line(trans('passwords.mail_line_1'))
+            ->action(trans('passwords.reset_password'), url(config('app.url').route('password.reset', $this->token, false)))
+            ->line(trans('passwords.mail_line_2'));
+    }
+}
