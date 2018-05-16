bf8bd980354a65ba3b7ff930f49eeeccc173f812
Saibamen <saibamenppl@gmail.com>
2018-05-16 21:11:57 +0200

    AdminTest

---
+++ tests/AdminTest.php
@@ -0,0 +1,143 @@
+<?php
+
+use App\Models\Reservation;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AdminTest extends BrowserKitTestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Session::start();
+
+        $user = factory(App\Models\User::class)->make(['is_admin' => true]);
+        $this->actingAs($user);
+    }
+
+    public function testIndex()
+    {
+        $this->visit('admin')
+            ->seePageIs('admin')
+            ->dontSee('Zaloguj')
+            ->see('Wszystkie rezerwacje')
+            ->see('Usuń')
+            ->see('Pokoje')
+            ->see('Goście')
+            ->see('Rezerwacje')
+            ->see('Pamiętaj o utworzeniu kopii zapasowej bazy danych!');
+
+        $user = factory(App\Models\User::class)->make();
+        $this->actingAs($user);
+
+        $this->visit('admin')
+            ->seePageIs('room')
+            ->dontSee('Pamiętaj o utworzeniu kopii zapasowej bazy danych!');
+    }
+
+    public function testDeleteRooms()
+    {
+        $reservation = factory(Reservation::class)->create();
+
+        $this->seeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->seeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->seeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+
+        $response = $this->call('DELETE', 'admin/deleteRooms', [
+            '_token' => csrf_token(),
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->notSeeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->notSeeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->seeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+    }
+
+    public function testDeleteGuests()
+    {
+        $reservation = factory(Reservation::class)->create();
+
+        $this->seeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->seeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->seeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+
+        $response = $this->call('DELETE', 'admin/deleteGuests', [
+            '_token' => csrf_token(),
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->notSeeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->seeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->notSeeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+    }
+
+    public function testDeleteReservations()
+    {
+        $reservation = factory(Reservation::class)->create();
+
+        $this->seeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->seeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->seeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+
+        $response = $this->call('DELETE', 'admin/deleteReservations', [
+            '_token' => csrf_token(),
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->notSeeInDatabase('reservations', [
+            'id' => $reservation->id,
+        ]);
+
+        $this->seeInDatabase('rooms', [
+            'id' => $reservation->room->id,
+        ]);
+
+        $this->seeInDatabase('guests', [
+            'id' => $reservation->guest->id,
+        ]);
+    }
+}
