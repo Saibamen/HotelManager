ef1203645a0f6c9a93528469a282f2a4da9d29db
Adam Stachowicz <adam.stachowicz@fingo.pl>
2018-05-20 17:53:20 +0200

    Unique numerify for Room factory number

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Room::class, function (Faker $faker) {
     return [
-        'number'   => $faker->numerify(),
+        'number'   => $faker->unique()->numerify(),
         'floor'    => $faker->numberBetween(-2, 50),
         'capacity' => $faker->numberBetween(1, 25),
         'price'    => $faker->randomFloat(),
