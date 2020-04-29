5d13e0ad0bb70581ddf6225b10572f35dc859a7b
Adam Stachowicz <saibamenppl@gmail.com>
2020-04-29 22:54:42 +0000

    Apply fixes from StyleCI

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Models\Reservation::class, function (Faker $faker) use ($fa
         'room_id'     => function ($post) use ($factory, $faker) {
             return $factory->create(App\Models\Room::class, [
                 'capacity' => $faker->numberBetween(isset($post['people']) ? (int) $post['people'] : 1, 99),
-                ])->id;
+            ])->id;
         },
         'guest_id'    => $factory->create(App\Models\Guest::class)->id,
         'date_start'  => $faker->date(),
